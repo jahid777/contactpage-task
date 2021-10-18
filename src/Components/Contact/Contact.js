f64201b5becba3ead52ca7b49da279f62e7d1203
unknown <za0162hid@gmail.com>
2021-10-19 00:46:05 +0600

    all done

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,16 +1,43 @@
-import React from "react";
+import React, { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
 import "./Contact.css";
 
 const Contact = () => {
+  useEffect(() => {
+    setTimeout(function () {
+      AOS.init();
+    }, 1000);
+  }, []);
   return (
-    <div className="text-center">
-      <h1 className="contact-heading">Contact</h1>
-      <p className="text-one">Start a new project with us</p>
-      <h2 className="head-one">projects@loftgarten.co</h2>
-      <p className="text-two">Press enquiries</p>
-      <h2 className="head-two">press@loftgarten.co</h2>
-      <p>
-        10–20 Gwynne <br /> Street Cremorne, <br /> Victoria Australia
+    <div className="text-center contact">
+      <div className="animation"></div>
+      <h1
+        className="contact-heading"
+        data-aos="fade-up"
+        data-aos-duration="500"
+      >
+        Contact
+      </h1>
+      <p className="text-one" data-aos="fade-up" data-aos-duration="1000">
+        Start a new project with us
+      </p>
+      <p
+        className="head-one hover-underline-animation"
+        data-aos="fade-up"
+        data-aos-duration="2000"
+      >
+        projects@loftgarten.co
+      </p>
+      <p className="text-two" data-aos="fade-up" data-aos-duration="3000">
+        Press enquiries
+      </p>
+      <p
+        className="head-two hover-underline-animation"
+        data-aos="fade-up"
+        data-aos-duration="3000"
+      >
+        press@loftgarten.co
       </p>
     </div>
   );
